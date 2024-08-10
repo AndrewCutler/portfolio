11cef0903af0327b2bc58bd52c9ccc9ff4b6c577
Andrew Cutler <AndrewKCutler@gmail.com>
2024-08-09 23:07:07 -0400

    removed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">Andrew Cutler</span>' +
 				'</div>'
 			)
 				.appendTo($body);
